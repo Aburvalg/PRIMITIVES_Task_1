e66b980e848ecbc90b2dca5ad296a1a1a3fd8667
Andrey Fedorov <95009754+Aburvalg@users.noreply.github.com>
2021-12-28 01:21:04 +0300

    Update Main_v2.java

--- src/Main_v2.java
+++ src/Main_v2.java
@@ -2,7 +2,7 @@ public class Main_v2 {
     public static void main(String[] args) {
 
         double PriceTicket = 12300.56;
-        int OneMile = 20;
+        int OneMile = 20; // для изменения стоимости мили
 
         if (PriceTicket > 0 & PriceTicket < 1000000) {
                        System.out.println("Ваши мили: " + (int) (PriceTicket / OneMile));
